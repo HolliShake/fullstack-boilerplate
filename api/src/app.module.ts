d56791dc3434c4c6e2fc3ffca78101f48acbe631
HolliShake <redondophilippandrewroa.dev@gmail.com>
2025-03-08 12:57:58 +0800

    Some update

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,20 +2,24 @@ import { Module } from '@nestjs/common';
 // Controller
 import { UserController } from '@/controllers/user.controller';
 import { RoleController } from '@controllers/role.controller';
+import { RoleActionController } from './controllers/role-action.controller';
 // Service
 import { PrismaModule } from './prisma/prisma.module';
 import { UserService } from '@services/user.service';
 import { RoleService } from '@services/role.service';
+import { RoleActionService } from './services/role-action.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [
     UserController,
-    RoleController
+    RoleController,
+    RoleActionController
   ],
   providers: [
     UserService,
-    RoleService
+    RoleService,
+    RoleActionService
   ],
 })
 
